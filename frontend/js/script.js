a1008b7b6a8f9d63c41a1e55d536559b4f7d3bcc
stlb57 <saarthaktyagi1@gmail.com>
2025-09-21 23:42:27 +0530

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,7 +1,7 @@
 // frontend/js/script.js
 
 document.addEventListener("DOMContentLoaded", () => {
-    const API_BASE_URL = "http://127.0.0.1:5000";
+    const API_BASE_URL = "https://veritas-project.onrender.com";
     let confidenceChart = null;
 
     // Page-specific logic
